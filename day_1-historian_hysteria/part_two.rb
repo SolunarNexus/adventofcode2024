a88dd3746697ec070c878af8b5d913482f6284a1
Oskar Adam Valent <oskar.valent@siko.cz>
2024-12-03 14:11:32 +0100

    Solution for part two

---
+++ day_1-historian_hysteria/part_two.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+left_lst = []
+right_lst = []
+occurrences = Hash.new(0)
+similarity_score = 0
+
+File.readlines('input', chomp: true).each do |line|
+  parts = line.split
+  left_lst.append(Integer(parts[0]))
+  right_lst.append(Integer(parts[1]))
+end
+
+right_lst.each do |right|
+  if occurrences.has_key? right
+    occurrences[right] += 1
+  else
+    occurrences[right] = 1
+  end
+end
+
+left_lst.each do |left|
+  similarity_score += left * occurrences[left]
+end
+
+puts "#{similarity_score}"
