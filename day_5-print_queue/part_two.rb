3e50cb5257407d54e6c7d47a287e783dadce73fb
SolunarNexus <adam.valent88@gmail.com>
2024-12-06 14:06:52 +0100

    refactor redundant array

--- day_5-print_queue/part_two.rb
+++ day_5-print_queue/part_two.rb
@@ -40,10 +40,13 @@ end
 
 def reorder_pages_correctly(pages, ordering_rules)
   new_pages = pages
-  pages.each_with_index do |page, i|
+
+  pages.each do |page|
     ordering_rules.fetch(page, []).each do |successor|
+      current_page_idx = pages.find_index(page)
       successor_idx = pages.find_index(successor)
-      if !successor_idx.nil? && pages.find_index(page) > successor_idx
+
+      if !successor_idx.nil? && current_page_idx > successor_idx
         new_pages.delete(page)
         new_pages.insert(successor_idx, page)
       end
@@ -53,18 +56,13 @@ def reorder_pages_correctly(pages, ordering_rules)
 end
 
 ordering_rules, manuals_to_produce = parse_input_file('input')
-incorrectly_ordered_manuals = []
 middle_pages_sum = 0
 
 manuals_to_produce.each do |pages|
-  if !is_correct_update(pages, ordering_rules)
-    incorrectly_ordered_manuals.append(pages)
+  unless is_correct_update(pages, ordering_rules)
+    correctly_ordered_manual = reorder_pages_correctly(pages, ordering_rules)
+    middle_pages_sum += Integer(correctly_ordered_manual[correctly_ordered_manual.length / 2])
   end
-end
-
-incorrectly_ordered_manuals.each do |pages|
-  correctly_ordered_manual = reorder_pages_correctly(pages, ordering_rules)
-  middle_pages_sum += Integer(correctly_ordered_manual[correctly_ordered_manual.length / 2])
 end
 
 # should output 6370
