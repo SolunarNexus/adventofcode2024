a0013512a5a8a10f6499814b6a9301c4ce3137fc
SolunarNexus <adam.valent88@gmail.com>
2024-12-09 14:16:04 +0100

    solution for part two

---
+++ day_7-bridge-repair/part_two.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/ruby
+
+def load_input(file_name)
+  equations = Array.new
+
+  File.readlines(file_name, chomp: true).each do |line|
+    parts = line.split(/:/)
+    test_value = Integer(parts[0])
+    parameters = parts[1].chomp.split.map { |part| Integer(part) }
+
+    unless equations.find(lambda {return nil}) { |e| e == test_value }.nil?
+      puts test_value
+    end
+    equations.append([test_value, parameters])
+  end
+  equations
+end
+
+def has_equation_solution(test_value, parameters, current_value)
+  if current_value > test_value
+    return false
+  end
+  if parameters.empty?
+    if current_value == test_value
+      return true
+    end
+    return false
+  end
+
+  has_equation_solution(test_value, parameters[1..], current_value + parameters[0]) ||
+    has_equation_solution(test_value, parameters[1..], current_value * parameters[0]) ||
+    has_equation_solution(test_value, parameters[1..], Integer(current_value.to_s + parameters[0].to_s))
+end
+
+equations = load_input('input')
+sum = 0
+
+equations.each do |equation|
+  test_value = equation[0]
+  parameters = equation[1]
+  if has_equation_solution(test_value, parameters[1..], parameters[0])
+    sum += test_value
+  end
+end
+
+# should output 362646859298554
+puts sum
