29c5e26adf118eaeb89bd955109d6d02c7238c7d
SolunarNexus <adam.valent88@gmail.com>
2024-12-11 09:41:22 +0100

    finally solve part two

--- day_9-disk_fragmenter/part_two.rb
+++ day_9-disk_fragmenter/part_two.rb
@@ -3,7 +3,7 @@
 disk_map = []
 id = 0
 
-File.read('test_input', chomp: true).each_char.with_index do |char, index|
+File.read('input', chomp: true).each_char.with_index do |char, index|
   if char == '0'
     next
   end
@@ -15,7 +15,6 @@ File.read('test_input', chomp: true).each_char.with_index do |char, index|
   end
 end
 
-left = 0
 right = disk_map.length - 1
 checksum = 0
 
@@ -27,7 +26,7 @@ while right > 0
 
   file_size = disk_map[right].length
 
-  while left < disk_map.length
+  while left < right # disk_map.length
     free_space_size = disk_map[left].length
     size_difference = free_space_size - file_size
 
@@ -42,14 +41,13 @@ while right > 0
       break
     end
 
-    left += 1
-
-    while left < disk_map.length && !disk_map[left][0].nil?
+    loop do
       left += 1
+      break unless left < disk_map.length && !disk_map[left][0].nil?
     end
   end
 
-  if left == disk_map.length
+  if left >= right
     right -= 1
   end
 
@@ -58,7 +56,13 @@ while right > 0
   end
 end
 
-puts "#{disk_map}"
+disk_map = disk_map.flatten
+
+disk_map.each_with_index do |block, index|
+  unless block.nil?
+    checksum += index * block
+  end
+end
 
 # should output 6326952672104
 puts checksum
