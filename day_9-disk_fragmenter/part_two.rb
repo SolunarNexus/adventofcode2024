73365ff2b5ff56c61d356a9b11e421f0cbfdd418
SolunarNexus <adam.valent88@gmail.com>
2024-12-11 10:20:27 +0100

    extract methods scanning disk map and moving file blocks

--- day_9-disk_fragmenter/part_two.rb
+++ day_9-disk_fragmenter/part_two.rb
@@ -38,33 +38,46 @@ def find_file_block(disk_map, left, right)
   right
 end
 
-def move_file_block_front(disk_map)
-  right = disk_map.length - 1
+def move_file_block(disk_map, file_size, left, right, size_difference)
+  if size_difference == 0
+    disk_map[left], disk_map[right] = disk_map[right], disk_map[left]
+  elsif size_difference > 0
+    disk_map.delete_at(left)
+    disk_map.insert(left, disk_map[right - 1])
+    disk_map.insert(left + 1, [nil] * size_difference)
+    disk_map[right + 1] = [nil] * file_size
+  end
+end
 
+def try_fit_file_block_in_some_free_block(disk_map, file_size, left, right)
+  while left < right
+    free_space_size = disk_map[left].length
+    size_difference = free_space_size - file_size
+
+    if size_difference >= 0
+      move_file_block(disk_map, file_size, left, right, size_difference)
+      break
+    end
+
+    left = find_free_block(disk_map, left + 1)
+  end
+  left
+end
+
+def inspect_all_file_blocks(disk_map, right)
   while right > 0
     left = find_free_block(disk_map)
     file_size = disk_map[right].length
 
-    while left < right # disk_map.length
-      free_space_size = disk_map[left].length
-      size_difference = free_space_size - file_size
-
-      if size_difference == 0
-        disk_map[left], disk_map[right] = disk_map[right], disk_map[left]
-        break
-      elsif size_difference > 0
-        disk_map.delete_at(left)
-        disk_map.insert(left, disk_map[right - 1])
-        disk_map.insert(left + 1, [nil] * size_difference)
-        disk_map[right + 1] = [nil] * file_size
-        break
-      end
-
-      left = find_free_block(disk_map, left + 1)
-    end
+    left = try_fit_file_block_in_some_free_block(disk_map, file_size, left, right)
 
     right = find_file_block(disk_map, left, right)
   end
+end
+
+def move_file_block_front(disk_map)
+  right = disk_map.length - 1
+  inspect_all_file_blocks(disk_map, right)
 end
 
 move_file_block_front(disk_map)
