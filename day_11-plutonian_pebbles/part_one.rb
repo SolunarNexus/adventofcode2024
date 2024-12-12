2243cf1e7556902471082f3d1751da0b7085eded
SolunarNexus <adam.valent88@gmail.com>
2024-12-12 21:32:49 +0100

    rename method

--- day_11-plutonian_pebbles/part_one.rb
+++ day_11-plutonian_pebbles/part_one.rb
@@ -2,39 +2,39 @@
 
 @cache = Hash.new
 
-def count(stone_number, blinks = 75)
-  if @cache.has_key? [stone_number,blinks]
-    return @cache[[stone_number,blinks]]
+def stones_count_after_n_blinks(stone, blinks = 75)
+  if @cache.has_key? [stone,blinks]
+    return @cache[[stone,blinks]]
   end
 
   if blinks == 0
     return 1
   end
 
-  if stone_number == 0
-    result = count(1, blinks - 1)
+  if stone == 0
+    result = stones_count_after_n_blinks(1, blinks - 1)
     @cache[[1, blinks-1]] = result
     return result
   end
 
-  length = Math.log10(stone_number).to_i + 1
+  length = Math.log10(stone).to_i + 1
 
   if length % 2 == 1
-    result = count(stone_number * 2024, blinks - 1)
-    @cache[[stone_number*2024,blinks-1]] = result
+    result = stones_count_after_n_blinks(stone * 2024, blinks - 1)
+    @cache[[stone*2024,blinks-1]] = result
     return result
   end
 
-  result1 = count(stone_number / 10 ** (length / 2), blinks - 1)
-  result2 = count(stone_number % 10 ** (length / 2), blinks - 1)
+  result1 = stones_count_after_n_blinks(stone / 10 ** (length / 2), blinks - 1)
+  result2 = stones_count_after_n_blinks(stone % 10 ** (length / 2), blinks - 1)
 
-  @cache[[stone_number / 10 ** (length / 2),blinks-1]] = result1
-  @cache[[stone_number % 10 ** (length / 2), blinks-1]] = result2
-  @cache[[stone_number,blinks]] = result1 + result2
+  @cache[[stone / 10 ** (length / 2),blinks-1]] = result1
+  @cache[[stone % 10 ** (length / 2), blinks-1]] = result2
+  @cache[[stone,blinks]] = result1 + result2
 
   result1+result2
 end
 
 stones = File.read('input', chomp: true).split.map { |stone| stone.to_i }
 # should output 232454623677743
-puts stones.map { |stone| count(stone) }.sum
+puts stones.map { |stone| stones_count_after_n_blinks(stone) }.sum
