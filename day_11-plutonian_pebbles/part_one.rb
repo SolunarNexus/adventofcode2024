a2b27f0ae015c0c664f7ca2fc84d448ea2f04330
SolunarNexus <adam.valent88@gmail.com>
2024-12-12 21:24:58 +0100

    implement solution using caching

--- day_11-plutonian_pebbles/part_one.rb
+++ day_11-plutonian_pebbles/part_one.rb
@@ -37,8 +37,8 @@ def blink(stone)
     if stone.number == 0
       stone.number = 1
     elsif (length = Math.log10(stone.number).to_i + 1) % 2 == 0
-      left_half = stone.number / (10 ** (length/2))
-      right_half = stone.number % (10 ** (length/2))
+      left_half = stone.number / (10 ** (length / 2))
+      right_half = stone.number % (10 ** (length / 2))
       stone.number = left_half
       stone.insert(right_half)
       stone = stone.next
@@ -61,15 +61,53 @@ def blink_n_times(stones, n)
   stones_count
 end
 
-stone = Stone.new
-head = stone
-numbers = File.read('input', chomp: true).split
+@cache = Hash.new
 
-numbers[1..-1].each.with_index(1) do |number, index|
-  stone.number = numbers[index-1].to_i
-  stone.next = Stone.new(number.to_i, nil)
-  stone = stone.next
+def count(x, d = 75)
+  if @cache.has_key? [x,d]
+    return @cache[[x,d]]
+  end
+
+  if d == 0
+    return 1
+  end
+
+  if x == 0
+    result = count(1, d - 1)
+    @cache[[1, d-1]] = result
+    return result
+  end
+
+  length = Math.log10(x).to_i + 1
+
+  if length % 2 == 1
+    result = count(x * 2024, d - 1)
+    @cache[[x*2024,d-1]] = result
+    return result
+  end
+
+  result1 = count(x / 10 ** (length / 2), d - 1)
+  result2 = count(x % 10 ** (length / 2), d - 1)
+
+  @cache[[x / 10 ** (length / 2),d-1]] = result1
+  @cache[[x % 10 ** (length / 2), d-1]] = result2
+  @cache[[x,d]] = result1 + result2
+
+  result1+result2
 end
 
-puts blink_n_times(head, 75)
-puts head
+# stone = Stone.new
+# head = stone
+
+stones = File.read('input', chomp: true).split.map { |stone| stone.to_i }
+stones = stones.map { |stone| count(stone) }
+puts "#{stones.sum}"
+
+# numbers[1..-1].each.with_index(1) do |number, index|
+#   stone.number = numbers[index - 1].to_i
+#   stone.next = Stone.new(number.to_i, nil)
+#   stone = stone.next
+# end
+#
+# puts blink_n_times(head, 75)
+# puts head
