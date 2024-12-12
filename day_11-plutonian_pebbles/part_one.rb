b967507707b7166398ebd0ffabd933d23f399182
SolunarNexus <adam.valent88@gmail.com>
2024-12-12 20:47:37 +0100

    improved solution, still inefficient

--- day_11-plutonian_pebbles/part_one.rb
+++ day_11-plutonian_pebbles/part_one.rb
@@ -1,34 +1,75 @@
 #!/usr/bin/ruby
 
-def blink(stones)
-  stones_after_blink = []
+class Stone
+  attr_accessor :number, :next
 
-  stones.each do |stone_number|
-    if stone_number == '0'
-      stones_after_blink.append '1'
-    elsif stone_number.length % 2 == 0
-      stones_after_blink.append stone_number[0..stone_number.length/2-1]
-      stones_after_blink.append stone_number[stone_number.length/2..].to_i.to_s
+  def initialize(num = nil, successor = nil)
+    @number = num
+    @next = successor
+  end
+
+  def insert(number)
+    if self.next.nil?
+      self.next = Stone.new(number)
     else
-      stones_after_blink.append (Integer(stone_number) * 2024).to_s
+      new_one = Stone.new(number)
+      self.next, new_one.next = new_one, self.next
     end
   end
-  stones_after_blink
+
+  def to_s
+    str = ""
+    node = self
+    loop do
+      str += node.number.to_s
+      break if node.next.nil?
+      str += ' '
+      node = node.next
+    end
+    str
+  end
+end
+
+def blink(stone)
+  stones_count = 0
+
+  until stone.nil?
+    if stone.number == 0
+      stone.number = 1
+    elsif (length = Math.log10(stone.number).to_i + 1) % 2 == 0
+      left_half = stone.number / (10 ** (length/2))
+      right_half = stone.number % (10 ** (length/2))
+      stone.number = left_half
+      stone.insert(right_half)
+      stone = stone.next
+      stones_count += 1
+    else
+      stone.number = stone.number * 2024
+    end
+    stones_count += 1
+    stone = stone.next
+  end
+  stones_count
 end
 
 def blink_n_times(stones, n)
-  # stones_copy = Marshal.load(Marshal.dump(stones))
-
+  stones_count = nil
   n.times do |i|
-    stones = blink(stones)
-    puts "#{i}: #{stones.length}"
+    stones_count = blink(stones)
+    puts "#{i}: #{stones_count}"
   end
-  stones
+  stones_count
 end
 
-stones = File.read('input', chomp:true).split
+stone = Stone.new
+head = stone
+numbers = File.read('input', chomp: true).split
 
-# after_25_blinks = blink_n_times(stones, 25)
-after_75_blinks = blink_n_times(stones, 75)
+numbers[1..-1].each.with_index(1) do |number, index|
+  stone.number = numbers[index-1].to_i
+  stone.next = Stone.new(number.to_i, nil)
+  stone = stone.next
+end
 
-puts after_25_blinks.length
+puts blink_n_times(head, 75)
+puts head
