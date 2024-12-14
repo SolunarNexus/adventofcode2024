bd1675a275a33696b55c2d174d44545c14a32566
SolunarNexus <adam.valent88@gmail.com>
2024-12-14 22:09:14 +0100

    extract machine parsing to its own function

--- day_13-claw_contraption/part_one.rb
+++ day_13-claw_contraption/part_one.rb
@@ -1,13 +1,20 @@
 #!/usr/bin/ruby
 
-def compute_min_tokens_needed(puzzle_input)
+def get_claw_machines_specifications(input)
+  specifications = []
+
+  input.split(/\n\n/).each do |machine|
+    groups = machine.scan(/\d+/)
+    specifications.append groups.map { |arg| arg.to_i }
+  end
+  specifications
+end
+
+def compute_min_tokens_needed(machines)
   total = 0
   tolerance = 0.0001
-  machines = puzzle_input.split(/\n\n/)
 
-  machines.each do |machine|
-    groups = machine.scan(/\d+/)
-    ax, ay, bx, by, x, y = groups.map { |arg| arg.to_i }
+  machines.each do |ax, ay, bx, by, x, y|
     a = (bx * y - by * x) / (bx * ay - by * ax).to_f
     b = (x - ax * a) / bx.to_f
     if (a - a.round).abs < tolerance and (b - b.round).abs < tolerance
@@ -18,6 +25,7 @@ def compute_min_tokens_needed(puzzle_input)
 end
 
 input = File.read('input', chomp: true)
-p1 = compute_min_tokens_needed(input)
+machines = get_claw_machines_specifications(input)
+p1 = compute_min_tokens_needed(machines)
 # should output 29201
 puts p1
