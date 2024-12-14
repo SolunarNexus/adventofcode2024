fe9e8864dc7d3b48791ced36ab78f5d2a7c65399
SolunarNexus <adam.valent88@gmail.com>
2024-12-14 22:06:33 +0100

    solution for part one

---
+++ day_13-claw_contraption/part_one.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+def compute_min_tokens_needed(puzzle_input)
+  total = 0
+  tolerance = 0.0001
+  machines = puzzle_input.split(/\n\n/)
+
+  machines.each do |machine|
+    groups = machine.scan(/\d+/)
+    ax, ay, bx, by, x, y = groups.map { |arg| arg.to_i }
+    a = (bx * y - by * x) / (bx * ay - by * ax).to_f
+    b = (x - ax * a) / bx.to_f
+    if (a - a.round).abs < tolerance and (b - b.round).abs < tolerance
+      total += 3 * a + b
+    end
+  end
+  total.round
+end
+
+p1 = compute_min_tokens_needed(File.read('input', chomp: true))
+# should output 29201
+puts p1
