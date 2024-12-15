f0d601946ce75ce287dba0dff875bea9d5c79fb6
SolunarNexus <adam.valent88@gmail.com>
2024-12-15 16:25:14 +0100

    implement loading input data

---
+++ day_14-restroom_redoubt/part_one.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+class Robot
+  attr_accessor :start_position
+  attr_accessor :velocity
+
+  def initialize(x, y, dx, dy)
+    @start_position = [x, y]
+    @velocity = [dx, dy]
+  end
+
+  def to_s
+    "p=#{@start_position[0]},#{@start_position[1]} v=#{@velocity[0]},#{@velocity[1]}"
+  end
+end
+
+def get_robots_from_input(filename)
+  pattern = /-?\d+/
+  robots = Array.new
+
+  File.readlines(filename).each do |line|
+    x, y, dx, dy = line.scan(pattern)
+    robots.append Robot.new(x, y, dx, dy)
+  end
+  robots
+end
+
+robots = get_robots_from_input('test_input')
+puts robots
