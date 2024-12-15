9c749c391b6b38ee21b4f8b14d5ffe916fd35cda
SolunarNexus <adam.valent88@gmail.com>
2024-12-15 20:52:13 +0100

    implement processing instructions

--- day_15-warehouse-woes/part_one.rb
+++ day_15-warehouse-woes/part_one.rb
@@ -27,8 +27,78 @@ def load_input(filename)
   [map, instructions, robot_position]
 end
 
+def try_move_boxes(x, y, instruction, map)
+  diff = [0, 0]
+
+  case instruction
+  when '^'
+    diff[1] -= 1
+  when '>'
+    diff[0] += 1
+  when '<'
+    diff[0] -= 1
+  when 'v'
+    diff[1] += 1
+  else
+    throw Exception("Invalid instruction: #{instruction}")
+  end
+
+  while true
+    x += diff[0]
+    y += diff[1]
+
+    if map[y][x] == Warehouse::EMPTY
+      map[y][x] = Warehouse::BOX
+      return true
+    elsif map[y][x] == Warehouse::WALL
+      return false
+    end
+  end
+end
+
+def move_robot(origin, instruction, map)
+  to_position = [origin[0], origin[1]]
+
+  case instruction
+  when '^'
+    to_position[1] -= 1
+  when '>'
+    to_position[0] += 1
+  when '<'
+    to_position[0] -= 1
+  when 'v'
+    to_position[1] += 1
+  else
+    throw Exception("Invalid instruction: #{instruction}")
+  end
+
+  x = to_position[0]
+  y = to_position[1]
+
+  if map[y][x] == Warehouse::WALL
+    return origin
+  end
+  if map[y][x] == Warehouse::EMPTY
+    map[origin[1]][origin[0]], map[y][x] = map[y][x], map[origin[1]][origin[0]]
+    return to_position
+  end
+  if map[y][x] == Warehouse::BOX && try_move_boxes(x, y, instruction, map)
+    map[origin[1]][origin[0]] = Warehouse::EMPTY
+    map[y][x] = '@'
+    return to_position
+  end
+  origin
+end
+
+def process_instructions(from_position, instructions, map)
+  instructions.each_char do |instruction|
+    from_position = move_robot(from_position, instruction, map)
+  end
+end
+
 map, instructions, robot = load_input('test_input2')
 
 puts map
 puts "#{instructions}"
-puts robot
+process_instructions(robot, instructions, map)
+puts map
