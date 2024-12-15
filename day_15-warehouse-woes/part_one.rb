9a788815d2212c8c8ad0d86baf9ea3077f29086b
SolunarNexus <adam.valent88@gmail.com>
2024-12-15 20:10:59 +0100

    output initial robot position

--- day_15-warehouse-woes/part_one.rb
+++ day_15-warehouse-woes/part_one.rb
@@ -9,19 +9,26 @@ end
 def load_input(filename)
   map = Array.new
   instructions = String.new
+  robot_position = nil
   input = File.read(filename, chomp: true).split(/\n\n/)
 
-  input[0].each_line(chomp: true) do |line|
+  input[0].each_line(chomp: true).with_index do |line, line_idx|
     map.append line
+
+    if robot_position.nil?
+      robot_x_pos = line.index('@')
+      robot_position = robot_x_pos.nil? ? nil : [line_idx, robot_x_pos]
+    end
   end
 
   input[1].each_line(chomp: true) do |line|
     instructions += line
   end
-  [map, instructions]
+  [map, instructions, robot_position]
 end
 
-map, instructions = load_input('test_input2')
+map, instructions, robot = load_input('test_input2')
 
 puts map
 puts "#{instructions}"
+puts robot
