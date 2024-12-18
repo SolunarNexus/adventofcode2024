74aafb13fbdf6614058909f75fe6d8a1218a80c3
SolunarNexus <adam.valent88@gmail.com>
2024-12-18 22:10:56 +0100

    add solution for both parts

---
+++ day_18-ram_run/solution.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/ruby
+
+require 'pqueue'
+require 'set'
+
+test = false
+
+if test
+  SIZE = 7
+  INPUT = 'test_input'
+  WAIT = 12
+else
+  SIZE = 71
+  INPUT = 'corrupted_coords'
+  WAIT = 1024
+end
+
+DIRECTIONS = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+def in_bounds(y, x) = y >= 0 && y < SIZE && x >= 0 && x < SIZE
+
+def make_map(coords)
+  grid = SIZE.times.map { Array.new(SIZE) }
+  coords.each_with_index { |(x,y),i| grid[y][x] = i+1 }
+  grid
+end
+
+def find_path(grid, start_time=0)
+  start = [0,0]
+  target = [SIZE-1,SIZE-1]
+
+  initial = [start, start_time]
+  queue = PQueue.new([initial]) { |a,b| a[0].sum - a[1] > b[0].sum - b[1] }
+  visited = Set[]
+
+  until queue.empty? do
+    pos,time = queue.pop
+    next unless visited.add? pos
+
+    if pos == target
+      return time - start_time
+    end
+
+    y,x = pos
+    DIRECTIONS.each { |dy,dx|
+      ny, nx = y+dy, x+dx
+      next unless in_bounds(ny,nx)
+
+      next_cell = grid[ny][nx]
+      next_time = time+1
+
+      next if next_cell && next_time >= next_cell
+      queue << [[ny,nx], next_time]
+    }
+  end
+end
+
+corrupted_coords = File.readlines(INPUT).map { |line| line.split(',').map(&:to_i) }
+
+# Part 1
+map = make_map(corrupted_coords.take(WAIT))
+# should output 380
+puts find_path(map, WAIT)
+
+
+# Part 2
+byte =  corrupted_coords.drop(WAIT).find.with_index { |(x,y),i|
+  map[y][x] = i+WAIT
+  !find_path(map, i+WAIT+1)
+}.join(",")
+# should output 26,50
+puts byte
