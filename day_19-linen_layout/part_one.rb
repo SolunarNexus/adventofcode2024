11c8d4d1754822977c6b990092d7a6c9fb7732cb
SolunarNexus <adam.valent88@gmail.com>
2024-12-19 15:51:41 +0100

    inefficient solution using recursion

--- day_19-linen_layout/part_one.rb
+++ day_19-linen_layout/part_one.rb
@@ -7,7 +7,30 @@ def load_input(filename)
   [towel_patterns, combinations]
 end
 
-towel_patterns, combinations = load_input 'test_input'
+def is_possible(patterns, combination)
+  if combination.nil? || patterns.empty?
+    return false
+  end
+  if combination.length == 0
+    return true
+  end
 
-puts "#{towel_patterns}"
-puts "#{combinations}"
+  viable_patterns = patterns.filter {|pattern| combination.start_with?(pattern)}
+
+  return viable_patterns.map {|pattern| is_possible(patterns, combination.sub(pattern, ""))}.include?(true)
+end
+
+def possible_combinations(patterns, combinations)
+  total = 0
+
+  combinations.each do |combination|
+    total += 1 if is_possible(patterns, combination)
+  end
+  total
+end
+
+towel_patterns, combinations = load_input 'input'
+
+total = possible_combinations(towel_patterns, combinations)
+
+puts total
